9690819f88ba250cc3736f18abbe69430475813a
elkrot <elkrot@gmail.com>
2022-05-28 23:05:58 +0300

    first commit

--- triangle.go
+++ triangle.go
@@ -1,6 +1,20 @@
 package golang_united_school_homework
 
+import (
+	"math"
+)
+
 // Triangle must satisfy to Shape interface
 type Triangle struct {
 	Side float64
+}
+
+func (t *Triangle) CalcPerimeter() float64 {
+	return t.Side * 3
+
+}
+
+// CalcArea returns calculation result of area
+func (t *Triangle) CalcArea() float64 {
+	return t.Side * t.Side * math.Sqrt(3) / 4
 }
